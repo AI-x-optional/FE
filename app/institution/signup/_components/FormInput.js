b432b34468b6f342600699d2c927160bfbe5909f
yunseo <cysdl5604@inu.ac.kr>
2024-05-13 23:37:59 +0900

    Feat: 폼 label, input 태그 컴포넌트화

---
+++ app/institution/signup/_components/FormInput.js
@@ -0,0 +1,21 @@
+export default function FormInput(props) {
+    const { label, type, name, autoComplete, placeholder } = props;
+
+    return (
+        <>
+            <label 
+                className="block text-sm font-medium leading-6 text-gray-900">
+                    {label}
+            </label>
+            <div className="mt-2">
+                <input
+                    type={type}
+                    name={name}
+                    autoComplete={autoComplete}
+                    className="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder={placeholder}
+                />
+            </div>
+        </>
+    );
+}
