1a4c5fd48d2a98924ba34702dd343acaa33b2a18
이효진 <hodin030@gmail.com>
2024-05-23 14:48:55 +0900

    [#19] feat: fetch API 구조 적용

--- app/bootcamp/[bootCampId]/page.js
+++ app/bootcamp/[bootCampId]/page.js
@@ -1,5 +1,17 @@
-export default function BootCampDetail() {
-    return(
+async function getBootCampDetail({ bootCampId }) {
+    const response = await fetch(`api 주소`);
+
+    if (response.ok) {
+        return await response.json()
+    } else {
+        Error("Data fetching error");
+    }
+}
+
+export default async function BootCampDetail({ params }) {
+    //const bootCampDetail = await getBootCampDetail(params.bootCampId);
+
+    return (
         <p>BootCampDetail 페이지</p>
     )
 }
