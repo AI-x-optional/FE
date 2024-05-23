aab28a3b3e4a514e0e44e00347a4abdd829f14fe
이효진 <hodin030@gmail.com>
2024-05-23 15:50:24 +0900

    [#19] style: 부트캠프 상세 내용 임시 데이터 적용

--- app/bootcamp/[bootCampId]/page.js
+++ app/bootcamp/[bootCampId]/page.js
@@ -15,21 +15,28 @@ export default async function BootCampDetail({ params }) {
     return (
         <div>
             <div className="mx-40 py-8">
-                <div>
-                    <div className="divide-y pr-12 h-screen">
+                <div className="divide-x">
+                    <div className="divide-y pr-12">
                         {/**부트캠프 제목*/}
                         <div className="space-y-4 pb-4">
-                            <div className="text-4xl font-bold">부트캠프 제목</div>
-                            <div className="">2024.03.21 ~ 2024.04.20</div>
+                            <div className="text-4xl font-bold">메타버스 아카데미 3기</div>
+                            <div class="flex items-center">
+                                <div class="p-1 bg-gray-200 rounded-md">모집 기간</div>
+                                <p class="ml-2">2024년 3월 20일 ~ 2024년 4월 30일</p>
+                            </div>
                         </div>
 
                         {/**부트캠프 내용*/}
                         <div className="space-y-12 pt-8">
                             {/**부트캠프 상세정보 */}
-                            <div className="">
-                                <div className="text-xl font-bold">교육기관</div>
-                                <div>과학기술정보통신부</div>
+                            {bootcampData.map((item) => (
+                            <div className="space-y-4">
+                                <div className="text-xl font-bold">{item.title}</div>
+                                <div class="w-full p-4 border border-gray-300 rounded-md">
+                                    {item.content}
+                                </div>
                             </div>
+                            ))}
                         </div>
                     </div>
                 </div>
@@ -37,5 +44,48 @@ export default async function BootCampDetail({ params }) {
         </div>
     );
 }
+
+const bootcampData = [
+    {
+        title: "과정 상세 설명",
+        content: "서버개발"
+    },
+    {
+        title: "교육기관",
+        content: "과학기술정보부"
+    },
+    {
+        title: "커리큘럼",
+        content: "커리큘럼 내용"
+    },
+    {
+        title: "분야",
+        content: "백엔드"
+    },
+    {
+        title: "교육비",
+        content: "무료"
+    },
+    {
+        title: "위치",
+        content: "경기"
+    },
+    {
+        title: "교육 기간",
+        content: "2024/06/17 ~ 2024/11/29"
+    },
+    {
+        title: "수업 시간",
+        content: "09:00 ~ 18:00"
+    },
+    {
+        title: "지원 절차",
+        content: "지원서 접수 > 예비학습 > 선수학습 > 정규과정"
+    },
+    {
+        title: "강사 정보",
+        content: "킹"
+    }
+]
 
 
