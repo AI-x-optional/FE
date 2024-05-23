5aa9a902afd95c4b098f71c98317f42499d76eb9
이효진 <hodin030@gmail.com>
2024-05-23 15:00:03 +0900

    [#19] style: 부트캠프 제목, 내용 ui

--- app/bootcamp/[bootCampId]/page.js
+++ app/bootcamp/[bootCampId]/page.js
@@ -9,9 +9,33 @@ async function getBootCampDetail({ bootCampId }) {
 }
 
 export default async function BootCampDetail({ params }) {
+
     //const bootCampDetail = await getBootCampDetail(params.bootCampId);
 
     return (
-        <p>BootCampDetail 페이지</p>
-    )
+        <div>
+            <div className="mx-40 py-8">
+                <div>
+                    <div className="divide-y pr-12 h-screen">
+                        {/**부트캠프 제목*/}
+                        <div className="space-y-4 pb-4">
+                            <div className="text-4xl font-bold">부트캠프 제목</div>
+                            <div className="">2024.03.21 ~ 2024.04.20</div>
+                        </div>
+
+                        {/**부트캠프 내용*/}
+                        <div className="space-y-12 pt-8">
+                            {/**부트캠프 상세정보 */}
+                            <div className="">
+                                <div className="text-xl font-bold">교육기관</div>
+                                <div>과학기술정보통신부</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
+
+
